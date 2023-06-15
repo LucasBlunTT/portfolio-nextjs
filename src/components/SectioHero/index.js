5d63e3023ebab20db877dd053dcfbc7caa7c7378
Lucas da Silva <sklucassilva@gmail.com>
2023-06-15 15:26:05 -0300

    metadata gerados

--- src/components/SectioHero/index.js
+++ src/components/SectioHero/index.js
@@ -17,10 +17,11 @@ export function SectionHero() {
                         Especialista em Desenvolvimento Front-end
                     </h1>
                     <p>Atuo a mais de 7 anos na área de Tecnologia, maior parte destes anos voltado para área de Health Care e Medicina Diagnóstica.  Durante minha jornada, trabalhei com as principais tecnologias de front-end (Reactjs e Nextjs), além de possuir sólidos conhecimentos em tecnologias de base (html, css, javascript). Essa experiência prática me permitiu desenvolver habilidades que me ajudam a criar soluções eficientes e eficazes.</p>
-                    <Link href='/'>
-                        Comece um projeto
-                        <Image src={Arrow} alt="imagem de seta pra direita" />
-                    </Link>
+                    {//<Link href='/'>
+                        //Comece um projeto
+                        //<Image src={Arrow} alt="imagem de seta pra direita" />
+                        //</Link>
+                    }
                 </div>
                 <div className="hero-right" data-aos="fade-left">
                     <Image src={LucasProfile} alt="Imagem Lucas" priority={true} />
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,21 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name="title" content="Portfolio Lucas da Silva" />
+        <meta name="description" content="Atuo a mais de 7 anos na área de Tecnologia, maior parte destes anos voltado para área de Health Care e Medicina Diagnóstica. Durante minha jornada, trabalhei com as principais tecnologias de front-end (Reactjs e Nextjs), além de possuir sólidos conhecimentos em tecnologias de base (html, css, javascript). Essa experiência prática me permitiu desenvolver habilidades que me ajudam a criar soluções eficientes e eficazes." />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.caslu.dev/" />
+        <meta property="og:title" content="Portfolio Lucas da Silva" />
+        <meta property="og:description" content="Atuo a mais de 7 anos na área de Tecnologia, maior parte destes anos voltado para área de Health Care e Medicina Diagnóstica. Durante minha jornada, trabalhei com as principais tecnologias de front-end (Reactjs e Nextjs), além de possuir sólidos conhecimentos em tecnologias de base (html, css, javascript). Essa experiência prática me permitiu desenvolver habilidades que me ajudam a criar soluções eficientes e eficazes." />
+        <meta property="og:image" content="https://www.caslu.dev/perfil.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.caslu.dev/" />
+        <meta property="twitter:title" content="Portfolio Lucas da Silva" />
+        <meta property="twitter:description" content="Atuo a mais de 7 anos na área de Tecnologia, maior parte destes anos voltado para área de Health Care e Medicina Diagnóstica. Durante minha jornada, trabalhei com as principais tecnologias de front-end (Reactjs e Nextjs), além de possuir sólidos conhecimentos em tecnologias de base (html, css, javascript). Essa experiência prática me permitiu desenvolver habilidades que me ajudam a criar soluções eficientes e eficazes." />
+        <meta property="twitter:image" content="https://www.caslu.dev/perfil.png" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
