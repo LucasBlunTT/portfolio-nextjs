7ef1d166dfe0f25af33a4860eeb867ae73217cc8
Lucas da Silva <sklucassilva@gmail.com>
2023-07-20 11:03:47 -0300

    atualizacao botão download curriculo

--- src/components/Curriculo/index.js
+++ src/components/Curriculo/index.js
@@ -3,10 +3,7 @@ import { CurriculoStyle } from './styles';
 
 export function Curriculo() {
   return (
-    <CurriculoStyle
-      type="submit"
-      href="../../../public/Lucas da Silva - CV.pdf"
-    >
+    <CurriculoStyle>
       <Link
         href={'https://www.caslu.dev/cv.pdf'}
         target="_blank"
